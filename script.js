6013e53ae74b22fd7f2f7a60c38cc0a816b58546
Niklas Solomon <niklassolomon@gmail.com>
2022-06-14 21:17:40 -0700

    Added character variables, OK messages, length confirmation, and arrays.

--- script.js
+++ script.js
@@ -2,14 +2,6 @@
 
 // Storing all needed characters in variables.
 
-var characterArray = [];
-var generatePassword = "";
-var length;
-var okUpper;
-var okLower;
-var okNumbers;
-var okSpecial;
-
 // numbers
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -27,26 +19,39 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 // Generate a password
 function generatePassword() {
 
+
+var generatePassword = "";
+var length;
+var okUpper;
+var okLower;
+var okNumbers;
+var okSpecial;
+
   // `Prompt` the user for their `passwordLength` and store it in a variable
   length = window.prompt("Enter desired password length.", "");
-
-  if (legnth >= 8 && length <= 128 ) {
+  
+  // Validate the password length and make sure it's a number between or equal to 8 and 128
+  // IF `passwordLength` is greater than or equal to 8 and less than or equal to 128
+  // IF `passwordLength` is less than 8 or greater than 128
+  
+  if (length >= 8 && length <= 128 ) {
     okNumbers = window.confirm("Would you like to use numbers?\nClick OK to confirm.");
     okSpecial = window.confirm("Would you like to use special characters?\nClick OK to confirm.");
     okLower = window.confirm("Would you like to use lowercase letters?\nClick OK to confirm.");
     okUpper = window.confirm("Would you like to use uppercase letters?\nClick OK to confirm.");
-  }
-
-  // Validate the password length and make sure it's a number between or equal to 8 and 128
-
-      // IF `passwordLength` is greater than or equal to 8 and less than or equal to 128
-
-      // IF `passwordLength` is less than 8 or greater than 128
-
-    if( true ) {
-      // end the function early
-      return "";
+  } else {
+      alert = "Please choose a password length between 8 and 128.";
     }
+  
+
+ 
+
+
+
+    // if( true ) {
+    // end the function early
+    //   return "";
+    // };
     
   // Prompt the user for if they want special characters and store it in a variable
 
@@ -57,17 +62,33 @@ function generatePassword() {
   // Prompt the user for if they want upper case and store it in a variable
 
   // Validate that at least one character choice is true
-    // if ( !var && !var && !var && !var) {
-    //   // Alert the user they had bad data
-    //   return;
-    // }
+    if ( !okNumbers && !okSpecial && !okLower && !okUpper) {
+       // Alert the user they had bad data
+      alert("Please select at least one type of character.")
+      return;
+    };
       
   // Declare new empty array to store the characters we want to use
+    var characterArray = [];
 
     // IF `specialCharacters` is true
+    if (okSpecial) {
+      //Combine `specialChar` with `characterArray`
+      characterArray = characterArray.concat(specialChar);
+    }
 
-    //Combine `specialCharacters` with `characters`
-    characterArray = characterArray.concat();
+    if (okNumbers) {
+      characterArray = characterArray.concat(numbers);
+    }
+
+    if (okLower) {
+      characterArray = characterArray.concat(lowercase);
+    }
+
+    if (okUpper) {
+      characterArray = characterArray.concat(uppercase);
+    }
+    
     //Array concat method to join together into one array
 
     // DECLARE a new empty string to store the password characters.
